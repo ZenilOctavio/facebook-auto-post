3d92d72ba0785fd23990200975f7962080d3f044
Octavio Zenil <octaviozenil@gmail.com>
2024-01-14 12:36:51 -0700

    Latest historical data with scrapping from yahoo

--- src/main.py
+++ src/main.py
@@ -1,20 +1,24 @@
 from datetime import datetime, timedelta
 from YahooFinance.YahooFinance import DefinedTicker, DefinedHistoryNames as DH
 from EventManagement.EventPoolSync import EventPoolSync
+from YahooScrapper.YahooScrapper import YahooScrapper
 
-tickers = ['HOMEX.MX', 'TV', 'TSLA', 'AAPL', 'KO']
+tickers = ['^GSPC', '^DJI', '^IXIC', 'MXN=X']
 
 ticker_objects: dict[str, DefinedTicker] = {}
+scrapper = YahooScrapper()
 
-for ticker in tickers:
-  try: 
-    ticker_objects[ticker] = DefinedTicker(ticker)
-  except :
-    print(f'Ticker not found: {ticker}')
+# for ticker in tickers:
+#   try: 
+#     ticker_objects[ticker] = DefinedTicker(ticker)
+#   except :
+#     print(f'Ticker not found: {ticker}')
 
 @EventPoolSync.repeat_event(datetime.now() + timedelta(seconds=5), timedelta(minutes=1))
 def print_yahoo_data_sync():
-  for ticker_object in ticker_objects.values():
-    print(f'{ticker_object.shortName} -> {ticker_object.today_data(columns= [DH.Open, DH.Close])}')
+  print(scrapper.get_today_data_from_tickers(tickers))
+
+
+
     
 EventPoolSync.run()
